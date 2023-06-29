84e9defcf53cbb7a76e2adc586cefcbfb2c220b6
Filipe Siqueira <fcsiqueira1409@gmail.com>
2023-06-29 11:34:56 -0300

    fix: fix diplay buggy

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, View, Text, Image } from 'react-native'
+import { SafeAreaView, View, Text, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
@@ -15,10 +15,9 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white p-5">
-      <Text>
 
         {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <View className="flex-row pb-2 items-center mx-4 space-x-2">
           <Image
             source={{
               uri: "https://links.papareact.com/wru"
@@ -36,7 +35,21 @@ const HomeScreen = () => {
           <Feather name="user" size={35} color="#00CCBB" />
         </View>
 
-      </Text>
+        {/* Search */}
+        <View className="flex-row items-center space-x-2 p-2 mx-2">
+          <View className="flex-row space-x-2 flex-1 bg-gray-100 p-3">
+            <Feather name="search" size={20} color="gray" />
+            <TextInput
+              placeholder='Restaurantes e Lanchonetes'
+              keyboardType='default'
+            />
+          </View>
+
+          <Ionicons name="filter" size={28} color="#00CCBB" />
+        </View>
+
+        {/* Content */}
+
     </SafeAreaView>
   )
 }
