be95b35b8c047da0847d93ebab91ba4c045fe21e
Filipe Siqueira <fcsiqueira1409@gmail.com>
2023-06-29 18:21:11 -0300

    feat: published bd

---
+++ sanity.js
@@ -0,0 +1,17 @@
+import { SanityClient } from "@sanity/client"
+import imageUrlBuilder from "@sanity/image-url"
+
+const client = SanityClient({
+  projectId: "uqaos55z",
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: "2023-06-29"
+})
+
+const builder = imageUrlBuilder(client)
+export const urlFor = (source) => builder.image(source)
+
+// RUN THIS to add exception for localhost 3000 CORS policy
+// sanity cors add http://localhost:19006
+
+export default client
