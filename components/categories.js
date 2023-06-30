c22b1e1a0f9d4a96ed2bbfb69ea7c521a9144f74
Filipe Siqueira <fcsiqueira1409@gmail.com>
2023-06-30 13:20:34 -0300

    feat: finish to implement backend in the home

--- components/categories.js
+++ components/categories.js
@@ -1,9 +1,23 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import CategoryCard from './categoryCard'
+import client, { urlFor } from '../sanity'
+import category from '../cornsilk-spider/schemas/category'
 
 const Categories = () => {
+  const [categories, setCategories] = useState()
+
+  useEffect(() => {
+    client.fetch(
+      ` 
+        *[_type == "category"]
+      `
+    ).then((data) => {
+      setCategories(data);
+    });
+  }, []);
+
   return (
     <ScrollView
       horizontal
@@ -14,12 +28,13 @@ const Categories = () => {
       }}
     >
       {/* Category Card */}
-      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 1" />
-      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 2" />
-      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 3" />
-      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 4" />
-      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 5" />
-      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 6" />
+      {categories?.map((category) => (
+        <CategoryCard 
+          key={category._id}
+          imgUrl={urlFor(category.image).width(200).url()} 
+          title={category.name}
+        />
+      ))}
     </ScrollView>
   )
 }
