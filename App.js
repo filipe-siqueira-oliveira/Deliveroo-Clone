710e25ddacbe6aa96cc37229989eaa502cd6eb27
Filipe Siqueira <fcsiqueira1409@gmail.com>
2023-06-30 16:58:30 -0300

    feat: implements the dishRows and Redux

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import 'react-native-url-polyfill/auto';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { Provider } from 'react-redux';
+import store from "./store"
+
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
 
@@ -10,10 +13,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Restaurant" component={RestaurantScreen}/>
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen}/>
+          <Stack.Screen name="Restaurant" component={RestaurantScreen}/>
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
